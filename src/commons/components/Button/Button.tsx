b35b60199255eae0c784f47301ea5d565dcc5b9f
oNqNu <onanukintaro@gmail.com>
2023-05-09 05:28:22 +0900

    fix: typo

--- src/commons/components/Button/Button.tsx
+++ src/commons/components/Button/Button.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import styles from '../Button/Button.module.css';
 import classnames from 'classnames';
 
-type BottomButtonProps = {
+type ButtonProps = {
   variant: 'primary' | 'secondary' | `danger`;
   onClick?: () => void;
   children?: React.ReactNode;
 };
 
-export const Button = (props: BottomButtonProps) => {
+export const Button = (props: ButtonProps) => {
   return (
     <button className={classnames(styles.button, styles[props.variant])}>
       {props.children}
