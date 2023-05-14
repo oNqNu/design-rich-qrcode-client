54ec1de8d33402d403abd85bce8245c6b62fcdf6
oNqNu <onanukintaro@gmail.com>
2023-05-14 21:00:55 +0900

    fix: devServerで立てるソースディレクトリを dist ⇨ build

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, 'dist'),
+      directory: path.join(__dirname, 'build'),
     },
     port: 3000,
   },
